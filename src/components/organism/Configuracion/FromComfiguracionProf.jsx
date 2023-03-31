25609da64e298f1bc51ec670ffb3cd095b749851
LuisAlejandro2003 <luisalejandro2003121822@gmail.com>
2023-03-31 03:39:08 -0600

    [FIX] impresion de datos corregida

--- src/components/organism/Configuracion/FromComfiguracionProf.jsx
+++ src/components/organism/Configuracion/FromComfiguracionProf.jsx
@@ -7,12 +7,13 @@ import regresar from '../../../assets/img/icons/regresar.png'
 import iconUser from '../../../assets/img/icons/user.png'
 import "../../../assets/styles/Configuracion/FromConfiguracionProf.css";
 import { useEffect, useState } from "react";
-// import {useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 
 
 function FromConfiguracionProf() {
     // const profe = useSelector(state => state.events);
-    
+    const user = useSelector(state => state.user)
+
     const [data, setData] = useState([]);
     useEffect(() => {
         async function fetchData() {
@@ -52,33 +53,35 @@ function FromConfiguracionProf() {
                 <ConfigRegresar src={regresar} msg="Ajustes"/>
                 <Separador/>
                 <div className='container-information-config-desk'>
+                    
                     <div className='container-profile'>
                         <div className='container-foto'>
                             <img src={iconUser} className="foto" alt="foto de perfil" />
                         </div>
                         <div className='container-name-typ'>
-                            <div>
-                                {data.map((item)=>(
-                                    <Label  msn={item.nombreP}/>
-                                ))} 
+                            <div>                    
+                                    <Label  msn={user.user.nombreP   }/>
+                                    <br></br>
+                                    <Label  msn={user.user.telefonoP  }/>
                             </div>
                             <div><Label  msn="Profesor"/></div>
+                          
                         </div>
                     </div>
                     <div className='container-information'>
                         <ConfigDatosDeskGen 
                             description="Datos de la cuenta"
                             field1="Correo" 
-                            write1="alan@gmail.com"
+                            write1= {user.user.gmailP}
                             field2="Contraseña"
                             write2="*********"/>
 
                         <ConfigDatosDeskGen 
                             description="Datos personales"
                             field1="Nombre y apellido" 
-                            write1="Alan Romeo Lopez"
+                            write1={user.user.nombreP}
                             field2="Telefono"
-                            write2="9733221220"/>            
+                            write2={user.user.telefonoP}/>            
                     </div>
                     <div className='content-button-salir'>
                         <ButtonSalir to="/filtro2" className="buttonSalir"/>
